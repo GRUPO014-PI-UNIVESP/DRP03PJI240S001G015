d2b2a1159928744d0f525f4c66063578ba22d46b
GRUPO014-PI-UNIVESP <1829240@aluno.univesp.br>
2024-08-20 20:54:43 -0300

    correções e atualizações

--- 06QuadroFuncionarios.php
+++ 06QuadroFuncionarios.php
@@ -44,41 +44,21 @@ $query_depto->execute();
                   </tr>
                 </thead>
                 <tbody style="height: 75%; font-size: 10px;">
-                  <?php while($nomeLista = $query_nome->fetch(PDO::FETCH_ASSOC)){ $id = $nomeLista['ID_FUNCIONARIO']; $_SESSION['id_edit'] = $id;?>
+                  <?php while($nomeLista = $query_nome->fetch(PDO::FETCH_ASSOC)){ $id1 = $nomeLista['ID_FUNCIONARIO']; $_SESSION['id_edit'] = $id1;?>
                   <tr>
                     <th style="width: 10%; text-align: center"> <?php echo $nomeLista['ID_FUNCIONARIO']; ?> </th>
                     <td style="width: 35%"                    > <?php echo $nomeLista['NOME_FUNCIONARIO'] . '<br>'; echo 'Tel: ' . $nomeLista['TELEFONE'] . ' ' .'[ ' . $nomeLista['CIDADE'] . ' ]'; ?> </td>
                     <td style="width: 20%"                    > <?php echo $nomeLista['DEPARTAMENTO'] . '<br>'; echo '[ ' . $nomeLista['CARGO'] . ' ]' ?> </td>
                     <td style="width: 10%"                    > <?php if(!empty($nomeLista['DATA_ADMISSAO'])){$dO = $nomeLista['DATA_ADMISSAO']; $DO = strtotime($dO); echo date('d/m/Y', $DO);}else{ echo 'Logado';} ?> </td>
                     <td style="width: 15%">
-                      <a class="btn btn-sm btn-info" href="<?php echo $acesso8 ?>?id=<?php echo $id ?>">
+                      <a class="btn btn-sm btn-info" href="<?php echo $acesso8 ?>?id=<?php echo $id1 ?>">
                         <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-pencil-fill' viewBox='0 0 16 16'>
                           <path d='M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.5.5 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11z'/>
                         </svg></a>
-                      <!-- Ativador do Modal -->
-                      <button type="button" class="btn btn-sm btn-danger" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                        <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-trash-fill' viewBox='0 0 16 16'>
-                          <path d='M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5M8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5m3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0'/>
-                        </svg>
-                      </button>
-                        <!-- Modal -->
-                        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                          <div class="modal-dialog modal-dialog-centered">
-                            <div class="modal-content">
-                              <div class="modal-header">
-                                <h1 class="modal-title fs-5" id="exampleModalLabel">Deletar Registro</h1>
-                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                              </div>
-                              <div class="modal-body">
-                                <p style="font-size: 15px; color: pink">Confirme a deleção do registro de <?php echo '[ ' . $nomeLista['NOME_FUNCIONARIO'] . ' ]' ?> ?</p>
-                              </div>
-                              <div class="modal-footer">
-                                <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Cancelar</button>
-                                <button type="button" class="btn btn-primary" onclick="location.href='<?php echo $acesso9 ?>?id=<?php echo $id ?>'">Confirmar</button>
-                              </div>
-                            </div>
-                          </div>
-                        </div>
+                      <a class="btn btn-sm btn-danger" href="<?php echo $acesso9 ?>?id=<?php echo $id1 ?>">
+                        <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-pencil-fill' viewBox='0 0 16 16'>
+                          <path d='M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.5.5 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11z'/>
+                        </svg></a>
                     </td>            
                   </tr><?php } ?>
                 </tbody>
@@ -101,41 +81,21 @@ $query_depto->execute();
                   </tr>
                 </thead>
                 <tbody style="height: 75%; font-size: 10px;">
-                <?php while($deptoLista = $query_depto->fetch(PDO::FETCH_ASSOC)){ $id = $deptoLista['ID_FUNCIONARIO']; $_SESSION['id_edit'] = $id;?>
+                <?php while($deptoLista = $query_depto->fetch(PDO::FETCH_ASSOC)){ $id2 = $deptoLista['ID_FUNCIONARIO']; $_SESSION['id_edit'] = $id2;?>
                   <tr>
                     <th style="width: 10%; text-align: center"> <?php echo $deptoLista['ID_FUNCIONARIO']; ?> </th>
                     <td style="width: 20%"                    > <?php echo $deptoLista['DEPARTAMENTO'] . '<br>'; echo '[ ' . $deptoLista['CARGO'] . ' ]' ?> </td>
                     <td style="width: 35%"                    > <?php echo $deptoLista['NOME_FUNCIONARIO'] . '<br>'; echo 'Tel: ' . $deptoLista['TELEFONE'] . ' ' .'[ ' . $deptoLista['CIDADE'] . ' ]'; ?> </td>
                     <td style="width: 10%"                    > <?php if(!empty($deptoLista['DATA_ADMISSAO'])){$dO = $deptoLista['DATA_ADMISSAO']; $DO = strtotime($dO); echo date('d/m/Y', $DO);}else{ echo 'Logado';} ?> </td>
                     <td style="width: 15%">
-                      <a class="btn btn-sm btn-info" href="<?php echo $acesso8 ?>?id=<?php echo $id ?>">
+                      <a class="btn btn-sm btn-info" href="<?php echo $acesso8 ?>?id=<?php echo $id2 ?>">
                         <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-pencil-fill' viewBox='0 0 16 16'>
                           <path d='M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.5.5 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11z'/>
                         </svg></a>
-                      <!-- Ativador do Modal -->
-                      <button type="button" class="btn btn-sm btn-danger" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                        <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-trash-fill' viewBox='0 0 16 16'>
-                          <path d='M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5M8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5m3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0'/>
-                        </svg>
-                      </button>
-                        <!-- Modal -->
-                        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                          <div class="modal-dialog modal-dialog-centered">
-                            <div class="modal-content">
-                              <div class="modal-header">
-                                <h1 class="modal-title fs-5" id="exampleModalLabel">Deletar Registro</h1>
-                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                              </div>
-                              <div class="modal-body">
-                                <p style="font-size: 15px; color: pink">Confirme a deleção do registro de <?php echo '[ ' . $deptoLista['NOME_FUNCIONARIO'] . ' ]' ?> ?</p>
-                              </div>
-                              <div class="modal-footer">
-                                <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Cancelar</button>
-                                <button type="button" class="btn btn-primary" onclick="location.href='<?php echo $acesso9 ?>?id=<?php echo $id ?>'">Confirmar</button>
-                              </div>
-                            </div>
-                          </div>
-                        </div>
+                      <a class="btn btn-sm btn-danger" href="<?php echo $acesso9 ?>?id=<?php echo $id2 ?>">
+                        <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-pencil-fill' viewBox='0 0 16 16'>
+                          <path d='M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.5.5 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11z'/>
+                        </svg></a>
                     </td>            
                   </tr><?php } ?>
                 </tbody>
--- 10DeletaFunc.php
+++ 10DeletaFunc.php
@@ -7,8 +7,29 @@ include_once 'EstruturaPrincipal.php';
 if(!empty($_GET['id'])){
 
   $id_del   = $_GET['id'];
-  $queryMsg = $connDB->prepare("DELETE FROM quadro_funcionarios WHERE ID_FUNCIONARIO = :id");
-  $queryMsg->bindParam(':id', $id_del, PDO::PARAM_INT);
-  $queryMsg->execute();
-  header('Location: 06QuadroFuncionarios.php');
+
+  $query_nome = $connDB->prepare("SELECT NOME_FUNCIONARIO FROM quadro_funcionarios WHERE ID_FUNCIONARIO = :id");
+  $query_nome->bindParam(':id', $id_del, PDO::PARAM_INT);
+  $query_nome->execute();
+  $rowID = $query_nome->fetch(PDO::FETCH_ASSOC);
+
+  $deletar = filter_input_array(INPUT_POST, FILTER_DEFAULT);
+
+  if(!empty($deletar)){
+    $queryMsg = $connDB->prepare("DELETE FROM quadro_funcionarios WHERE ID_FUNCIONARIO = :id");
+    $queryMsg->bindParam(':id', $id_del, PDO::PARAM_INT);
+    $queryMsg->execute();
+
+    header('Location: 06QuadroFuncionarios.php');
+  }
 }
+?>
+<div class="main">
+  <div class="container">
+    <form method="POST" action="">
+      <p style="margin-left: 5%; margin-top: 5%; font-size: 16px">Deseja deletar o registro de <?php echo '[ ' . $rowID['NOME_FUNCIONARIO'] . ' ]' ?> </p>
+      <input class="btn btn-primary" style="width: 100px;margin-left: 20%" type="submit" id="submit" name="submit" value="Confirmar">
+      <input class="btn btn-danger"  style="width: 100px" type="reset" id="reset" name="reset" value="Cancelar" onclick="location.href='06QuadroFuncionarios.php'">
+    </form>
+  </div>
+</div>
