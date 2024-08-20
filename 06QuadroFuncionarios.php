5561700bf0fd9633562d73562fda71ede5acf9bb
GRUPO014-PI-UNIVESP <1829240@aluno.univesp.br>
2024-08-20 19:48:34 -0300

    correções e atualizações

--- 06QuadroFuncionarios.php
+++ 06QuadroFuncionarios.php
@@ -108,14 +108,34 @@ $query_depto->execute();
                     <td style="width: 35%"                    > <?php echo $deptoLista['NOME_FUNCIONARIO'] . '<br>'; echo 'Tel: ' . $deptoLista['TELEFONE'] . ' ' .'[ ' . $deptoLista['CIDADE'] . ' ]'; ?> </td>
                     <td style="width: 10%"                    > <?php if(!empty($deptoLista['DATA_ADMISSAO'])){$dO = $deptoLista['DATA_ADMISSAO']; $DO = strtotime($dO); echo date('d/m/Y', $DO);}else{ echo 'Logado';} ?> </td>
                     <td style="width: 15%">
-                      <a class="btn btn-sm btn-info" href="08EditaRegistroFuncionario.php?id=<?php echo $id ?>">
+                      <a class="btn btn-sm btn-info" href="<?php echo $acesso8 ?>?id=<?php echo $id ?>">
                         <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-pencil-fill' viewBox='0 0 16 16'>
                           <path d='M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.5.5 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11z'/>
                         </svg></a>
-                      <a class="btn btn-sm btn-danger" href="19DeletaFunc.php?id="<?php echo $id ?>>
+                      <!-- Ativador do Modal -->
+                      <button type="button" class="btn btn-sm btn-danger" data-bs-toggle="modal" data-bs-target="#exampleModal">
                         <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-trash-fill' viewBox='0 0 16 16'>
                           <path d='M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5M8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5m3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0'/>
-                        </svg></a>
+                        </svg>
+                      </button>
+                        <!-- Modal -->
+                        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                          <div class="modal-dialog modal-dialog-centered">
+                            <div class="modal-content">
+                              <div class="modal-header">
+                                <h1 class="modal-title fs-5" id="exampleModalLabel">Deletar Registro</h1>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                              </div>
+                              <div class="modal-body">
+                                <p style="font-size: 15px; color: pink">Confirme a deleção do registro de <?php echo '[ ' . $deptoLista['NOME_FUNCIONARIO'] . ' ]' ?> ?</p>
+                              </div>
+                              <div class="modal-footer">
+                                <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Cancelar</button>
+                                <button type="button" class="btn btn-primary" onclick="location.href='<?php echo $acesso9 ?>?id=<?php echo $id ?>'">Confirmar</button>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
                     </td>            
                   </tr><?php } ?>
                 </tbody>
