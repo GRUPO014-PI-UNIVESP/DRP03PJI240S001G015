a166e114f16ee101e0d285c3860b49758a09b990
GRUPO014-PI-UNIVESP <1829240@aluno.univesp.br>
2024-09-26 11:48:35 -0300

    continuação da construção dos Kanban's

--- app/00SeletorAdministrativo.php
+++ app/00SeletorAdministrativo.php
@@ -21,7 +21,7 @@
     }
     function resetTimer() {
       clearTimeout(time);
-        time = setTimeout(deslogar, 300000);
+        time = setTimeout(deslogar, 3000000);
     }
   };
   inactivityTime();
@@ -72,62 +72,70 @@
               $produtos = $connDB->prepare("SELECT * FROM pf_pedido");
               $produtos->execute();
 
-              while($rowPedido = $produtos->fetch(PDO::FETCH_ASSOC)){ ?>
-                <div class="card text-bg-success mb-3" style="width: 50rem;">
-                  <div class="card-body">
-                    <div class="row g-2">
-                      <div class="col-md-3">
-                        <div class="input-group mb-3"><span class="input-group-text" id="basic-addon1" style="font-size: 12px; background: rgba(0,0,0,0.3); color: aqua">Pedido No.</span>
-                          <input type="text" class="form-control" aria-label="" aria-describedby="basic-addon1" style="font-weight:bold; font-size: 14px; text-align: center; background: none;"
-                                 value="<?php echo $rowPedido['NUMERO_PEDIDO']?>" readonly>
+              while($rowPedido = $produtos->fetch(PDO::FETCH_ASSOC)){
+                if(!empty($rowPedido['NUMERO_PEDIDO'])){ ?>
+                  <div class="card text-bg-success mb-3" style="width: 50rem;">
+                    <div class="card-body">
+                      <div class="row g-2">
+                        <div class="col-md-3">
+                          <div class="input-group mb-3"><span class="input-group-text" id="basic-addon1" style="font-size: 12px; background: rgba(0,0,0,0.3); color: aqua">Pedido No.</span>
+                            <input type="text" class="form-control" aria-label="" aria-describedby="basic-addon1" style="font-weight:bold; font-size: 14px; text-align: center; background: none;"
+                                   value="<?php echo $rowPedido['NUMERO_PEDIDO']?>" readonly>
+                          </div>
                         </div>
-                      </div>
-                      <div class="col-md-9">
-                        <div class="input-group mb-3"><span class="input-group-text" id="basic-addon1" style="font-size: 12px; background: rgba(0,0,0,0.3); color: aqua">Produto</span>
-                          <input type="text" class="form-control" aria-label="" aria-describedby="basic-addon1" style="font-weight:bold; font-size: 14px; background: none"
-                                 value="<?php echo $rowPedido['NOME_PRODUTO'] ?>" readonly>
+                        <div class="col-md-9">
+                          <div class="input-group mb-3"><span class="input-group-text" id="basic-addon1" style="font-size: 12px; background: rgba(0,0,0,0.3); color: aqua">Produto</span>
+                            <input type="text" class="form-control" aria-label="" aria-describedby="basic-addon1" style="font-weight:bold; font-size: 14px; background: none"
+                                   value="<?php echo $rowPedido['NOME_PRODUTO'] ?>" readonly>
+                          </div>
                         </div>
-                      </div>
-                      <div class="col-md-3">
-                        <div class="input-group mb-3"><span class="input-group-text" id="basic-addon1" style="font-size: 12px; background: rgba(0,0,0,0.3); color: aqua">Quantidade</span>
-                          <input type="text" class="form-control" aria-label="" aria-describedby="basic-addon1" style="font-weight:bold; font-size: 14px; text-align: right; background: none"
-                                 value="<?php echo $rowPedido['QTDE_LOTE_PF'] . ' ' . $rowPedido['UNIDADE_MEDIDA'] ?>" readonly>
+                        <div class="col-md-3">
+                          <div class="input-group mb-3"><span class="input-group-text" id="basic-addon1" style="font-size: 12px; background: rgba(0,0,0,0.3); color: aqua">Quantidade</span>
+                            <input type="text" class="form-control" aria-label="" aria-describedby="basic-addon1" style="font-weight:bold; font-size: 14px; text-align: right; background: none"
+                                   value="<?php echo $rowPedido['QTDE_LOTE_PF'] . ' ' . $rowPedido['UNIDADE_MEDIDA'] ?>" readonly>
+                          </div>
                         </div>
-                      </div>
-                      <div class="col-md-9">
-                        <div class="input-group mb-3"><span class="input-group-text" id="basic-addon1" style="font-size: 12px; background: rgba(0,0,0,0.3); color: aqua">Cliente</span>
-                          <input type="text" class="form-control" aria-label="" aria-describedby="basic-addon1" style="font-weight:bold; font-size: 14px; background: none"
-                                 value="<?php echo $rowPedido['CLIENTE'] ?>" readonly>
+                        <div class="col-md-9">
+                          <div class="input-group mb-3"><span class="input-group-text" id="basic-addon1" style="font-size: 12px; background: rgba(0,0,0,0.3); color: aqua">Cliente</span>
+                            <input type="text" class="form-control" aria-label="" aria-describedby="basic-addon1" style="font-weight:bold; font-size: 14px; background: none"
+                                   value="<?php echo $rowPedido['CLIENTE'] ?>" readonly>
+                          </div>
                         </div>
-                      </div>
-                      <div class="col-md-3">
-                        <div class="input-group mb-3"><span class="input-group-text" id="basic-addon1" style="font-size: 12px; background: rgba(0,0,0,0.3); color: aqua">Uso da Planta</span>
-                          <?php $tempo = $rowPedido['QTDE_LOTE_PF'] / $rowPedido['CAPACIDADE_PROCESS']; $id = $rowPedido['NUMERO_PEDIDO'];?>
-                          <input type="text" class="form-control" aria-label="" aria-describedby="basic-addon1" style="font-weight:bold; font-size: 14px; text-align: center; background: none"
-                                 value="<?php echo round($tempo) . ' horas'?>" readonly>
+                        <div class="col-md-3">
+                          <div class="input-group mb-3"><span class="input-group-text" id="basic-addon1" style="font-size: 12px; background: rgba(0,0,0,0.3); color: aqua">Uso da Planta</span>
+                            <?php 
+                            if($rowPedido['CAPACIDADE_PROCESS'] != 0 || $rowPedido['CAPACIDADE_PROCESS'] != null){
+                              $tempo = $rowPedido['QTDE_LOTE_PF'] / $rowPedido['CAPACIDADE_PROCESS']; $id = $rowPedido['NUMERO_PEDIDO'];?>
+                              <input type="text" class="form-control" aria-label="" aria-describedby="basic-addon1" style="font-weight:bold; font-size: 14px; text-align: center; background: none"
+                                     value="<?php echo round($tempo) . ' horas'?>" readonly><?php
+                            } else { $tempo = 0; ?> 
+                              <input type="text" class="form-control" aria-label="" aria-describedby="basic-addon1" style="font-weight:bold; font-size: 14px; text-align: center; background: none"
+                              value="0" readonly>
+                            <?php } ?>
+                          </div>
                         </div>
-                      </div>
-                      <div class="col-md-3">
-                        <div class="input-group mb-3"><span class="input-group-text" id="basic-addon1" style="font-size: 12px; background: rgba(0,0,0,0.3); color: aqua">Fabricação</span>
-                          <input type="text" class="form-control" aria-label="" aria-describedby="basic-addon1" style="font-weight:bold; font-size: 14px; text-align: center; background: none"
-                                 value="<?php echo date('d/m/Y',strtotime($rowPedido['DATA_FABRI'])) ?>" readonly>
+                        <div class="col-md-3">
+                          <div class="input-group mb-3"><span class="input-group-text" id="basic-addon1" style="font-size: 12px; background: rgba(0,0,0,0.3); color: aqua">Fabricação</span>
+                            <input type="text" class="form-control" aria-label="" aria-describedby="basic-addon1" style="font-weight:bold; font-size: 14px; text-align: center; background: none"
+                                   value="<?php echo date('d/m/Y',strtotime($rowPedido['DATA_FABRI'])) ?>" readonly>
+                          </div>
                         </div>
-                      </div>
-                      <div class="col-md-3">
-                        <div class="input-group mb-3"><span class="input-group-text" id="basic-addon1" style="font-size: 12px; background: rgba(0,0,0,0.3); color: aqua">Entrega</span>
-                          <input type="text" class="form-control" aria-label="" aria-describedby="basic-addon1" style="font-weight:bold; font-size: 14px; text-align: center; background: none"
-                                 value="<?php echo date('d/m/Y',strtotime($rowPedido['DATA_ENTREGA'])) ?>" readonly>
+                        <div class="col-md-3">
+                          <div class="input-group mb-3"><span class="input-group-text" id="basic-addon1" style="font-size: 12px; background: rgba(0,0,0,0.3); color: aqua">Entrega</span>
+                            <input type="text" class="form-control" aria-label="" aria-describedby="basic-addon1" style="font-weight:bold; font-size: 14px; text-align: center; background: none"
+                                   value="<?php echo date('d/m/Y',strtotime($rowPedido['DATA_ENTREGA'])) ?>" readonly>
+                          </div>
                         </div>
-                      </div>
-                      <div class="col-md-3">
-                        <button class="btn btn-danger" style="font-size: 14px; float: right" onclick="location.href='./36CancelaPedido.php?id=<?php echo $id ?>'">Cancelar Pedido</button>
-                      </div>
-                      <div class="col-md-12" style="background: rgba(0,0,0,0.3); border-radius: 5px;">
-                        <h6 style="color: orange;">Situação : <?php echo $rowPedido['SITUACAO_QUALI'] ?></h6>
-                      </div>
-                    </div><!-- fim da DIV row g2 -->
-                  </div><!-- fim da DIV do corpo do cartão -->
-                </div><!-- fim da DIV do cartão --><?php
+                        <div class="col-md-3">
+                          <button class="btn btn-danger" style="font-size: 14px; float: right" onclick="location.href='./36CancelaPedido.php?id=<?php echo $id ?>'">Cancelar Pedido</button>
+                        </div>
+                        <div class="col-md-12" style="background: rgba(0,0,0,0.3); border-radius: 5px;">
+                          <h6 style="color: orange;">Situação : <?php echo $rowPedido['SITUACAO_QUALI'] ?></h6>
+                        </div>
+                      </div><!-- fim da DIV row g2 -->
+                    </div><!-- fim da DIV do corpo do cartão -->
+                  </div><!-- fim da DIV do cartão --><?php
+                }
               } ?><!-- fim da recursão -->
             </div><!-- fim da coluna exclusiva dos cartões -->
           </div><!-- fim da DIV row do setor comercial -->
