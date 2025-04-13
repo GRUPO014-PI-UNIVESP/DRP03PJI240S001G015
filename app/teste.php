8e738aa02e0ed1890ae774d9b7e2cedd85787c53
EDSON MASSAO MATSUUCHI <164646530+GRUPO014-PI-UNIVESP@users.noreply.github.com>
2025-04-12 21:53:58 -0300

    revisão XXII

--- app/teste.php
+++ app/teste.php
@@ -32,20 +32,54 @@
   //definição de hora local
 date_default_timezone_set('America/Sao_Paulo');
 ?>
-
 <!-- Área Principal -->
   <div class="main">
     <?php
-      $dataHoje = date('Y-m-d H:i');$dataAntes = date('Y-m-d H:i', strtotime("- 35minutes"));
+      $busca = $connDB->prepare("SELECT * FROM historico_tempo WHERE ID_TEMPO = 4");
+      $busca->execute(); $row = $busca->fetch(PDO::FETCH_ASSOC);
 
-      $dH = datetime::createFromFormat('Y-m-d H:i', $dataHoje);
-      $dA = datetime::createFromFormat('Y-m-d H:i', $dataAntes);
-      echo ''. $dataHoje .'<br>';
-      echo $dataAntes . '<br>';
-      $dif = $dA->diff($dH);
-      $a = $dif->format('%i minutos');
-      echo $a;
+      $dataC = new datetime($row['T_COMPRA']); $dataI = new datetime($row['INICIO']); $diferenca = $dataI->diff($dataC);
+      echo $diferenca->y . 'anos ' . $diferenca->m . 'meses ' . $diferenca->d . 'dias ' . $diferenca->h . 'horas ' . $diferenca->i . 'minutos <br>';
+      $compra = ($dataC->getTimestamp() - $dataI->getTimestamp()) / 60;
+      echo 'a compra foi entre ' . date('d/m/Y H:i', strtotime($row['INICIO'])) . ' até ' . date('d/m/Y H:i', strtotime($row['T_COMPRA'])) . ' são ' . $compra . 'minutos <br><br>'; 
 
-        ?>
+      $dataR = new datetime($row['T_RECEBE']); $dataC = new datetime($row['T_COMPRA']); $diferenca = $dataC->diff($dataR);
+      echo $diferenca->y . 'anos ' . $diferenca->m . 'meses ' . $diferenca->d . 'dias ' . $diferenca->h . 'horas ' . $diferenca->i . 'minutos <br>';
+      $recebe = ($dataR->getTimestamp() - $dataC->getTimestamp()) / 60;
+      echo 'o recebimento foi entre ' . date('d/m/Y H:i', strtotime($row['T_COMPRA'])) . ' até ' . date('d/m/Y H:i', strtotime($row['T_RECEBE'])) . ' são ' . $recebe . 'minutos <br><br>'; 
+
+      $dataAM = new datetime($row['T_ANAMAT']); $dataR = new datetime($row['T_RECEBE']); $diferenca = $dataR->diff($dataAM);
+      echo $diferenca->y . 'anos ' . $diferenca->m . 'meses ' . $diferenca->d . 'dias ' . $diferenca->h . 'horas ' . $diferenca->i . 'minutos <br>';
+      $anaMat = ($dataAM->getTimestamp() - $dataR->getTimestamp()) / 60;
+      echo 'a análise de material foi entre ' . date('d/m/Y H:i', strtotime($row['T_RECEBE'])) . ' até ' . date('d/m/Y H:i', strtotime($row['T_ANAMAT'])) . ' são ' . $anaMat . 'minutos <br><br>'; 
+
+      $dataF = new datetime($row['T_FABRI']); $dataAM = new datetime($row['T_ANAMAT']); $diferenca = $dataAM->diff($dataF);
+      echo $diferenca->y . 'anos ' . $diferenca->m . 'meses ' . $diferenca->d . 'dias ' . $diferenca->h . 'horas ' . $diferenca->i . 'minutos <br>';
+      $fabri = ($dataF->getTimestamp() - $dataAM->getTimestamp()) / 60;
+      echo 'a fabricação foi entre ' . date('d/m/Y H:i', strtotime($row['T_ANAMAT'])) . ' até ' . date('d/m/Y H:i', strtotime($row['T_FABRI'])) . ' são ' . $fabri . 'minutos <br><br>'; 
+
+      $dataF = new datetime($row['T_FABRI']); $dataAP = new datetime($row['T_ANAPRO']); $diferenca = $dataF->diff($dataAP);
+      echo $diferenca->y . 'anos ' . $diferenca->m . 'meses ' . $diferenca->d . 'dias ' . $diferenca->h . 'horas ' . $diferenca->i . 'minutos <br>';
+      $anaPro = ($dataAP->getTimestamp() - $dataF->getTimestamp()) / 60;
+      echo 'a análise do produto foi entre ' . date('d/m/Y H:i', strtotime($row['T_FABRI'])) . ' até ' . date('d/m/Y H:i', strtotime($row['T_ANAPRO'])) . ' são ' . $anaPro . 'minutos <br><br>'; 
+      
+      $dataE = new datetime($row['T_ENTREGA']); $dataAP = new datetime($row['T_ANAPRO']); $diferenca = $dataAP->diff($dataE);
+      echo $diferenca->y . 'anos ' . $diferenca->m . 'meses ' . $diferenca->d . 'dias ' . $diferenca->h . 'horas ' . $diferenca->i . 'minutos <br>';
+      $entrega = ($dataE->getTimestamp() - $dataAP->getTimestamp()) / 60;
+      echo 'a entrega foi entre ' . date('d/m/Y H:i', strtotime($row['T_ANAPRO'])) . ' até ' . date('d/m/Y H:i', strtotime($row['T_ENTREGA'])) . ' são ' . $entrega . 'minutos <br><br>';
+
+      $total = $compra + $recebe + $anaMat + $fabri + $anaPro + $entrega;
+
+      $grava = $connDB->prepare("UPDATE historico_tempo SET COMPRA = :compra, RECEBIMENTO = :recebe, ANALISE_MATERIAL = :anaMat, FABRICACAO = :fabri, ANALISE_PRODUTO = :anaPro, ENTREGA = :entrega, TOTAL = :total 
+                                        WHERE ID_TEMPO = 4");
+      $grava->bindParam(":compra" , $compra , PDO::PARAM_INT);
+      $grava->bindParam(":recebe" , $recebe , PDO::PARAM_INT);
+      $grava->bindParam(":anaMat" , $anaMat , PDO::PARAM_INT);
+      $grava->bindParam(":fabri"  , $fabri  , PDO::PARAM_INT);
+      $grava->bindParam(":anaPro" , $anaPro , PDO::PARAM_INT);
+      $grava->bindParam(":entrega", $entrega, PDO::PARAM_INT);
+      $grava->bindParam(":total"  , $total  , PDO::PARAM_INT);
+      $grava->execute();
+      ?>
 
   </div>
